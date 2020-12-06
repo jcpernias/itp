eda3238f9c431710fef98f8800c024141b45e8aa
José C. Pernías <jcpernias@gmail.com>
2020-12-06 16:51:25 +0100

    Fix package documentation

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' Household data from the ECV for the years 2005, 2011, and 2019.
 #'
-#' @format A [tibble](tibble::tibble-package).
+#' @format A tibble (see [tibble::tibble-package]).
 #' \describe{
 #'   \item{id_hh}{Household identifier.}
 #'   \item{ecv_year}{The year the survey was conducted. Variables refer to
@@ -16,7 +16,7 @@
 #'
 #' Personal data from the ECV for the years 2005, 2011, and 2019.
 #'
-#' @format A [tibble](tibble::tibble-package).
+#' @format A tibble (see [tibble::tibble-package]).
 #'
 #' @source \href{https://ine.es/dyngs/INEbase/en/operacion.htm?c=Estadistica_C&cid=1254736176807&menu=resultados&secc=1254736195153&idp=1254735976608#!tabs-1254736195153}{Microdata at INE, the Spanish Statistical Office}
 'individuals'
